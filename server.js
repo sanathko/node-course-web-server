3727d60a37c163de49695cd831b377f55b029cd0
sanathkodikara <sanath.kodikara@gmail.com>
2017-03-20 22:49:33 +1100

    initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+//reuse content as partials
+hbs.registerPartials(__dirname + '/views/partials');
+
+//view engine is handlebars
+app.set('view engine', 'hbs');
+
+//register logger
+app.use((req, res, next) => {
+  var now =  new Date().toString();
+  var log = `${now}: ${req.method}: ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+//overrides any page..goes only to this..
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+//static resources middlewear
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'Welcome to our page'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMesage: 'Unable to process'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('server is up on port 3000');
+});
